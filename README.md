7e576fb3d9e29162241c81ddc70cee807a2939c5
Shubham Pandey <shubhritik@gmail.com>
2020-06-09 22:12:49 +0530

    Final push of all the files. Currently in working state

---
+++ README.md
@@ -0,0 +1,14 @@
+Hey!
+I made this game, while I was learning Vanilla JavaScript. It is a very simple and small game. 
+
+About Game:
+1. It is a 2 player game.
+2. The UI of the game is created with HTML and CSS.
+3. The working of the game is done with the help of JavaScript.
+4. Anyone can use this code and manipulate it as they want.
+5. From my end this game is complete and I might not work on it futher as from my vision and skill set I have added all the things that I wanted.
+
+Pull Request: 
+One thing I would like to add in this game is Single player vs the CPU. But currently, it is out of my skill set. If anyone wants to add it, then feel free to fork and update it and then pull request.
+
+PEACE!
---
+++ app.js
@@ -0,0 +1,42 @@
+const container = document.querySelector('.container');
+let intro = 1;
+let arr = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10];
+container.addEventListener('click', function(event) {
+    const contID = event.target.parentNode.id;
+    if (contID !== '') {
+        let box = document.getElementById(contID);
+        let contIDS = contID.split('-');
+        if (intro <= 9) {
+            if (intro % 2 !== 0 || intro === 1) {
+                box.firstChild.src = 'cross.png';
+                arr[contIDS[1]] = 1;
+                if ((arr[1] === arr[2] && arr[2] === arr[3]) || (arr[4] === arr[5] && arr[5] === arr[6]) || (arr[7] === arr[8] && arr[8] === arr[9]) || (arr[1] === arr[4] && arr[4] === arr[7]) || (arr[2] === arr[5] && arr[5] === arr[8]) || (arr[3] === arr[6] && arr[6] === arr[9]) || (arr[1] === arr[5] && arr[5] === arr[9]) || (arr[3] === arr[5] && arr[5] === arr[7])) {
+                    console.log('Player1 Winner');
+                    document.querySelector('.boxes').style.display = 'none';
+                    let coIn = document.querySelector('.container');
+                    coIn.insertAdjacentHTML('afterbegin', '<h2>Player 1 Won, Refresh To play Again!</h2><button class="btn" onClick="window.location.reload();">Replay</button>');
+                }
+                document.getElementById(contID).removeAttribute('id');
+                intro++;
+                if (intro > 9) {
+                    document.querySelector('.drawBtn').insertAdjacentHTML('beforebegin', '<h2>Match Draw!</h2><button class="btn" onClick="window.location.reload();">Replay</button>');
+                }
+            } else {
+                box.firstChild.src = 'circle.png';
+                arr[contIDS[1]] = 0;
+
+                if ((arr[1] === arr[2] && arr[2] === arr[3]) || (arr[4] === arr[5] && arr[5] === arr[6]) || (arr[7] === arr[8] && arr[8] === arr[9]) || (arr[1] === arr[4] && arr[4] === arr[7]) || (arr[2] === arr[5] && arr[5] === arr[8]) || (arr[3] === arr[6] && arr[6] === arr[9]) || (arr[1] === arr[5] && arr[5] === arr[9]) || (arr[3] === arr[5] && arr[5] === arr[7])) {
+                    console.log('Player2 Winner');
+                    document.querySelector('.boxes').style.display = 'none';
+                    let coIn = document.querySelector('.container');
+                    coIn.insertAdjacentHTML('afterbegin', '<h2>Player 1 Won, Refresh To play Again!</h2><button class="btn" onClick="window.location.reload();">Replay</button>');
+                }
+                document.getElementById(contID).removeAttribute('id');
+                intro++;
+                if (intro > 9) {
+                    document.querySelector('.drawBtn').insertAdjacentHTML('beforebegin', '<h2>Match Draw!</h2><button class="btn" onClick="window.location.reload();">Replay</button>');
+                }
+            }
+        }
+    }
+});
